fb51f66ddcd454df8b5afc512f01818863842613
ShubhamGupta2505 <shubham3nemo@gmail.com>
2023-04-27 16:37:53 +0530

    Completing

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain,SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
 
 os.environ['OPENAI_API_KEY'] = apikey
@@ -9,9 +13,45 @@ os.environ['OPENAI_API_KEY'] = apikey
 
 st.title('🦜🔗 Youtube GPT Creator')
 prompt = st.text_input('Plug in your prompt here')
+title_template = PromptTemplate(
+    input_variables=['topic'],
+    template='Write me a youtube video title about {topic}'
+)
+
+script_template = PromptTemplate(
+    input_variables=['title','wikipedia_research'],
+    template='Write me a youtube video script based on this title TITLE : {title} while leveraging this wikipedia research : {wikipedia_research}'
+)
+title_memory = ConversationBufferMemory(input_key='topic',memory_key='chat_history')
+script_memory = ConversationBufferMemory(input_key='title',memory_key='chat_history')
+
 
 llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm,prompt=title_template,verbose =True,output_key='title',memory=title_memory)
+script_chain = LLMChain(llm=llm,prompt=script_template,verbose =True,output_key='script',memory=script_memory)
+
+
+# sequential_chain = SequentialChain(chains=[title_chain,script_chain],input_variables=['topic'],output_variables=['title','script'],verbose=True)
+
+wiki = WikipediaAPIWrapper()
 
 if prompt:
-    response = llm(prompt)
-    st.write(response)
+    title = title_chain.run(prompt)
+    wiki_research = wiki.run(prompt)
+    script = script_chain.run(title=title,wikipedia_research = wiki_research)
+
+    st.write(title)
+    st.write(script)
+
+
+    with st.expander('Title History'):
+        st.info(title_memory.buffer)
+
+    with st.expander('Script History'):
+        st.info(script_memory.buffer)
+
+
+    with st.expander('Wikipedia Research'):
+        st.info(wiki_research)
+
+
